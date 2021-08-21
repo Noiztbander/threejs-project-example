39bfd3e5d130816d48bf7b4c158f67eccb249706
Erick Noiztbander <78412093+Noiztbander@users.noreply.github.com>
2021-08-21 23:51:57 +0200

    Guitar animation done

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import gsap from "gsap";
+
+const gltfLoader = new GLTFLoader();
 
 // Debug
 const gui = new dat.GUI();
@@ -12,19 +16,31 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+let tl = gsap.timeline();
+
+// the guitar
+gltfLoader.load("3d-models/guitarrisha/guitarrisha.glb", (guitar) => {
+  guitar.scene.scale.set(0.2, 0.2, 0.2);
+  guitar.scene.position.set(0, 3, -3);
+  scene.add(guitar.scene);
+  gui.add(guitar.scene.rotation, "x").min(0).max(9);
+  gui.add(guitar.scene.rotation, "y").min(0).max(9);
+  gui.add(guitar.scene.rotation, "z").min(0).max(9);
+
+  tl.to(guitar.scene.scale, {x:0.3, y:0.3, z:0.3, duration: 2});
+  tl.to(guitar.scene.rotation, { y: 6.4, duration: 2 }, "-=2");
+  tl.to(guitar.scene.position, {x:-1}, "-=2");
+  tl.to(guitar.scene.position, {x:2, duration:2});
+});
+
 // Objects
-const geometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
 
 // Materials
-const material = new THREE.MeshBasicMaterial();
-material.color = new THREE.Color(0xff0000);
 
 // Mesh
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
 
 // Lights
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
+const pointLight = new THREE.PointLight(0xffffff, 1);
 pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
@@ -57,14 +73,14 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   sizes.width / sizes.height,
   0.1,
   100
 );
 camera.position.x = 0;
 camera.position.y = 0;
-camera.position.z = 2;
+camera.position.z = 5;
 scene.add(camera);
 
 // Controls
@@ -76,6 +92,7 @@ scene.add(camera);
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -90,7 +107,6 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  sphere.rotation.y = 0.5 * elapsedTime;
 
   // Update Orbital Controls
   // controls.update()
